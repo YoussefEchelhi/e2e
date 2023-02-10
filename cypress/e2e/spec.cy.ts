5035d3164ee143ccd34aafe8a956726485ff652f
YoussefEchelhi <114957292+YoussefEchelhi@users.noreply.github.com>
2023-02-10 14:32:56 +0100

    Added tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,38 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+beforeEach(() =>{
+  cy.visit("/");
+});
+
+  it('input exist', () => {
+    cy.get('input').should('exist');
   })
-})
+
+  it('input placeholder', () => {
+    cy.get('input').invoke('attr', 'placeholder').should('contain', 'Skriv titel här')   
+  });
+
+  it('button exist', () => {
+    cy.get('button').contains('Sök').should('exist');
+  })
+
+  it("type in input", () => {
+    cy.get("input").type("Lord").should("have.value", "Lord");
+  });
+
+  it("show error text when input is empty", ()=> {
+    cy.get("button").click();
+    cy.get("p").contains("Inga sökresultat att visa").should("exist");
+  });
+
+  it("show error text when seraching other thing than movies ", () => {
+    cy.get("input").type("Medieinstitutet").should("have.value", "Medieinstitutet")
+    cy.get("button#search").click();
+    cy.get("p").contains("Inga sökresultat att visa").should("exist")
+  });
+
+  it("should get 10 movies from API ", ()=> {
+    cy.get("input").type("lord").should("have.value","lord");
+    cy.get("button").click();
+    cy.get("div>div").should("have.length", 10);
+  });
+  
+
